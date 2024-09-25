1c99041a2b75317d8e1a31a4afab160c009df174
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-09-24 19:08:07 -0700

    attempted a speedup, didn't work. kept code as comment

--- shortestroute2.cpp
+++ shortestroute2.cpp
@@ -17,6 +17,16 @@ int main() {
     vector<vector<long long>> dist(n + 1, vector<long long>(n + 1, INF));
     for (int src = 1; src <= n; ++src) {
         dist[src][src] = 0;
+        /* attempted speed up that isn't giving the right answers.
+        for(int i = 1; i<=n-1; i++){
+            for(auto e: adj[i]){
+                //from a to b has weight w;
+                int b, w;
+                tie(b, w) = e;
+                dist[src][i] = min(dist[src][i], (long long)w);
+            }
+        }
+        */
         for (int i = 0; i < n - 1; ++i) {
             for (int u = 1; u <= n; ++u) {
                 if (dist[src][u] == INF) continue;
