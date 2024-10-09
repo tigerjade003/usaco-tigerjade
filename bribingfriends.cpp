ccfb7860bc777056770a2c6de40fbdc6f1a47cf6
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-10-08 20:20:29 -0700

    created bribingfriends.cpp

---
+++ bribingfriends.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n";
+
+void setIO(string file = "") {
+  cin.tie(0)->sync_with_stdio(0);
+  if ((int)(file.size())) {
+    freopen((file + ".in").c_str(), "r", stdin);
+    freopen((file + ".out").c_str(), "w", stdout);
+  }
+}
