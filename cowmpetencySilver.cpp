3153c012437f59b26d52250314b1d4f9f15f2e4e
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-10-29 20:55:04 -0700

    added some segtree stuff

--- cowmpetencySilver.cpp
+++ cowmpetencySilver.cpp
@@ -8,6 +8,43 @@ void setIO(string file = "") {
     freopen((file + ".out").c_str(), "w", stdout);
   }
 }
+vector<int> t;
+void build(int v, int tl, int tr){
+    if(tl == tr){
+        t[v] = nums[tl];
+    }
+    else{
+        int tm = (tl + tr)/2;
+        build(v*2, tl, tm);
+        build(v * 2 + 1, tm + 1, tr);
+        t[v] = t[v * 2] + t[v * 2 + 1]; 
+    }
+}
+void update(int v, int tl, int tr, int pos, int new_val){
+    if(tl == tr){
+        t[v] = new_val;
+    }
+    else{
+        int tm = (tl + tr)/2;
+        if(pos <= tm){
+            update(v * 2, tl, tm, pos, new_val);
+        }
+        else{
+            update(v * 2 + 1, tm + 1, tr, pos, new_val);
+        }
+        t[v] = t[v * 2] + t[v * 2 + 1];
+    }
+}
+int query(int v, int tl, int tr, int l, int r){
+    if(l > r){
+        return 0;
+    }
+    if(l == tl && r == tr){
+        return t[v];
+    }
+    int tm = (tl + tr) / 2;
+    return query(v * 2, tl, tm, l, min(r, tm)) + query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
+}
 int main(){
     int T;
     cin >> T;
@@ -15,6 +52,6 @@ int main(){
         int N, Q, C;
         cin >> N >> Q >> C;
         vector<int> sofar(N, 0);
-        vector<int> tree(4*N, 0);
+        t.assign(4*N, 0);
     }
 }
