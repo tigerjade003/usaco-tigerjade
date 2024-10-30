4f7314cf980e78d34f63ebdec55878c9a1568e51
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-10-29 20:54:17 -0700

    edited

--- cowmpetencySilver.cpp
+++ cowmpetencySilver.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define int long long
 void setIO(string file = "") {
   cin.tie(0)->sync_with_stdio(0);
   if ((int)(file.size())) {
     freopen((file + ".in").c_str(), "r", stdin);
     freopen((file + ".out").c_str(), "w", stdout);
   }
+}
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N, Q, C;
+        cin >> N >> Q >> C;
+        vector<int> sofar(N, 0);
+        vector<int> tree(4*N, 0);
+    }
 }
