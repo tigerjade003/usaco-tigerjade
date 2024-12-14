b4948da267588909a32f550a8c356c9c3741aec0
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-13 18:56:19 -0800

    wip

--- dance.cpp
+++ dance.cpp
@@ -25,19 +25,20 @@ int main(){
     }
     int count = n;
     vector<bool> istrue(n, true);
-    for(int i = 0; i<n; i++){
-        if(cangoto[i].size() < k){
-            count--;
-            istrue[i] = false;
-        }
-    }
     vector<int> cur(n, 0);
     for(int i = 0; i<n; i++){
-        cur[i] = i;
+        cur[i] = nums[i];
+    }
+    vector<int> lastsize(n, 0);
+    for(int i = 0; i<n; i++){
+        lastsize[i] = cangoto[i].size();
     }
     while(count > 0){
+        vector<int> cur(n, 0);
         for(int j = 0; j<n; j++){
+                
             
+
         }
     }
 }
