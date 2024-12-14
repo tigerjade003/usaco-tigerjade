155fa375fe773d54a9a9b45ada5bf80a26d3768c
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-13 18:45:25 -0800

    wip

--- dance.cpp
+++ dance.cpp
@@ -7,7 +7,22 @@ int main(){
     for(int i = 0; i<n; i++){
         nums[i] = i;
     }
-    for(int i = 0; i<k; i++){
-        
+    vector<set<int>> cangoto(n, set<int>());
+    for(int i = 0; i<n; i++){
+        cangoto[i].emplace(i);
     }
+    for(int i = 0; i<k; i++){
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        int temp;
+        temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+        cangoto[nums[a]].emplace(b);
+        cangoto[nums[b]].emplace(a);
+    }
+    int count = n;
+
 }
