db36398e99314a94ec368ca78cb7c4ee746affc0
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-14 11:01:24 -0800

    work in progress

--- redblackforest.cpp
+++ redblackforest.cpp
@@ -9,6 +9,11 @@ signed main(){
     cin >> T;
     while(T--){
         int N, K;
+        vector<int> location(N);
+        for(int i = 0; i<N; i++){
+            cin >> location[i];
+        }
+        sort(location.begin(), location.end());
         
     }
 }
