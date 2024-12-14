4183abf0935d399071bff737ad739fc613336e2e
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-14 12:01:04 -0800

    .

--- redblackforest.cpp
+++ redblackforest.cpp
@@ -47,7 +47,6 @@ signed main(){
             laws[i].start = news[laws[i].start];
             laws[i].end = news[laws[i].end];
         }
-        //determine the number of trees we need to stand
         vector<int> prefixss(compress+1, 0);
         for(int i = 0; i<K; i++){
             prefixss[laws[i].start]++;
@@ -66,41 +65,4 @@ signed main(){
         int needed = 0;
 
     }
-}
-vector<int> t;
-void build(int v, int tl, int tr){
-    if(tl == tr){
-        t[v] = nums[tl];
-    }
-    else{
-        int tm = (tl + tr)/2;
-        build(v*2, tl, tm);
-        build(v * 2 + 1, tm + 1, tr);
-        t[v] = t[v * 2] + t[v * 2 + 1]; 
-    }
-}
-void update(int v, int tl, int tr, int pos, int new_val){
-    if(tl == tr){
-        t[v] = new_val;
-    }
-    else{
-        int tm = (tl + tr)/2;
-        if(pos <= tm){
-            update(v * 2, tl, tm, pos, new_val);
-        }
-        else{
-            update(v * 2 + 1, tm + 1, tr, pos, new_val);
-        }
-        t[v] = t[v * 2] + t[v * 2 + 1];
-    }
-}
-int query(int v, int tl, int tr, int l, int r){
-    if(l > r){
-        return 0;
-    }
-    if(l == tl && r == tr){
-        return t[v];
-    }
-    int tm = (tl + tr) / 2;
-    return query(v * 2, tl, tm, l, min(r, tm)) + query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
 }
