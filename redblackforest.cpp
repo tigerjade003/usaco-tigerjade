34220af7fd9dc35b785956bb99639e7b00a7df82
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-14 11:10:05 -0800

    work in progress

--- redblackforest.cpp
+++ redblackforest.cpp
@@ -3,7 +3,9 @@ using namespace std;
 #define endl '\n'
 #define int long long
 int N;
-
+struct law{
+    int start, end, min;
+};
 signed main(){
     int T;
     cin >> T;
@@ -14,6 +16,12 @@ signed main(){
             cin >> location[i];
         }
         sort(location.begin(), location.end());
-        
+        //if possible to not cut overlap, don't cut overlap.
+        vector<law> laws;
+        for(int i = 0; i<K; i++){
+            int a, b, c;
+            cin >> a >> b >> c;
+            laws[i] = {a, b, c};
+        }
     }
 }
