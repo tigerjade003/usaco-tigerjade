4b357b34ab7d59210520770d605d2cae95899047
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-13 18:38:02 -0800

    working on dance.cpp

---
+++ dance.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+int main(){
+    cin >> n >> k;
+    vector<int> nums(n, 0);
+    for(int i = 0; i<n; i++){
+        nums[i] = i;
+    }
+    for(int i = 0; i<k; i++){
+        
+    }
+}
