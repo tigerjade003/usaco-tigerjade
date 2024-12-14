d9dc56dd93b25760f11ac9520fee0385e380e9ab
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-13 21:47:01 -0800

    undid it as it failed a testcase

--- towers.cpp
+++ towers.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-//#define int long long
+#define int long long
 void setIO(string file = "") {
   cin.tie(0)->sync_with_stdio(0);
   if ((int)(file.size())) {
