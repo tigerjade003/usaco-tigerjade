5537ba5f322f7d844c70e7f2084878fe696cb5a9
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-14 21:24:46 -0800

    working on it

--- belts.cpp
+++ belts.cpp
@@ -16,12 +16,35 @@ struct belt{
     short x, y, dir;
 };
 vector<belt> belts;
+vector<vector<bool>> visited;
+bool dfs(int x, int y){
+    if(x < 0 || y < 0 || x >= N || y >= N || grid[x][y] == 0) return false;
+    if(visited[x][y]) return true;
+    visited[x][y] = true;
+    int x1 = x;
+    int y1 = y;
+    if(grid[x][y] == 1){
+        y1--;
+    }
+    else if(grid[x][y] == 2){
+        y1++;
+    }
+    else if(grid[x][y] == 3){
+        x1--;
+    }
+    else{
+        x1++;
+    }
+    return dfs(x1, y1);
+}
 int main(){
     setIO();
     cin >> N >> Q;
     grid.assign(N, vector<short>(N, 0));
     belts.assign(Q, {0, 0, 0});
     for(int i = 0; i<N; i++){
-
+        short a, b;
+        char c;
+        
     }
 }
