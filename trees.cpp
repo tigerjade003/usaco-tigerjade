2316ce6ae1063f631689bea125fa74b772a10c26
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-25 18:11:22 -0800

    finished trees

--- trees.cpp
+++ trees.cpp
@@ -1,67 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define DEBUG false
 void setIO(string file = "") {
-    cin.tie(0)->sync_with_stdio(0);
-    if (!file.empty()) {
-        freopen((file + ".in").c_str(), "r", stdin);
-        freopen((file + ".out").c_str(), "w", stdout);
-    }
+  cin.tie(0)->sync_with_stdio(0);
+  if ((int)(file.size())) {
+    freopen((file + ".in").c_str(), "r", stdin);
+    freopen((file + ".out").c_str(), "w", stdout);
+  }
 }
-struct rule{
-    int begin, end, id;
+struct Event {
+    int l, type, r, cut;
 };
-struct thing{
-    int start, end, id, val;
-};
-bool comp(const thing &a, const thing &b){
-    if(a.start == b.start) return a.val < b.val;
-    return a.start < b.start;
+bool comp(const Event& a, const Event& b) {
+    if (a.l == b.l) return a.type < b.type;
+    return a.l < b.l;
 }
-void solve(){
-    int N, Q;
-    cin >> N >> Q;
-    vector<int> locations(N);
-    for(int i = 0; i<N; i++){
-        cin >> locations[i];
-    }
-    sort(locations.begin(), locations.end());
-    multiset<thing, decltype(&comp)> all(comp);
-    for(int i = 0; i<Q; i++){
-        int l, r, t;
-        cin >> l >> r >> t;
-        int existing = upper_bound(locations.begin(), locations.end(), r) - lower_bound(locations.begin(), locations.end(), l);
-        all.insert({l, -1, r, existing - t});
-    }
-    sort(locations.begin(), locations.end());
-    for(int i = 0; i<N; i++){
-        all.insert({locations[i], 0, 0, 0});
-    }
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    int ans = 0;
-    for (auto [l, type, r, cut] : all) {
-        if (type == -1) {
-            pq.push({ans + cut, r});
-        } else {
-            while (!pq.empty() && pq.top().second < l) {
-                pq.pop();
-            }
-            if (pq.empty() || pq.top().first != ans) {
-                ans++;
-            }
-        }
-    }
-    cout << ans << endl;
-}
-int main(){
-    if(DEBUG)
-        setIO("test");
-    else{
-        setIO();
-    }
+int main() {
+    setIO();
     int T;
     cin >> T;
-    while(T--){
-        solve();
+
+    while (T--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> x(n);
+        for (int i = 0; i < n; i++) {
+            cin >> x[i];
+        }
+        sort(x.begin(), x.end());
+
+        vector<Event> events;
+        for (int i = 0; i < n; i++) {
+            events.push_back({x[i], 0, 0, 0});
+        }
+        for (int i = 0; i < k; i++) {
+            int l, r, t;
+            cin >> l >> r >> t;
+            int existing = upper_bound(x.begin(), x.end(), r) - lower_bound(x.begin(), x.end(), l);
+            events.push_back({l, -1, r, existing - t});
+        }
+        sort(events.begin(), events.end(), comp);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int ans = 0;
+
+        for (auto [l, type, r, cut] : events) {
+            if (type == -1) {
+                pq.push({ans + cut, r});
+            } else {
+                while (!pq.empty() && pq.top().second < l) {
+                    pq.pop();
+                }
+                if (pq.empty() || pq.top().first != ans) {
+                    ans++;
+                }
+            }
+        }
+
+        cout << ans << endl;
     }
+
+    return 0;
 }
