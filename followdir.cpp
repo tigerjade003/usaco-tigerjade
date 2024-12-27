3af553ceeded97ad9df5790bab6612d10b220891
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2024-12-27 15:07:34 -0800

    done with bakery and fixed followdir

--- followdir.cpp
+++ followdir.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define DEBUG false
 void setIO(string file = "") {
   cin.tie(0)->sync_with_stdio(0);
   if ((int)(file.size())) {
@@ -9,13 +10,11 @@ void setIO(string file = "") {
   }
 }
 int N;
-vector<vector<bool>> dir; //false = R, true = D
+vector<vector<bool>> dir, visited; //false = R, true = D
 vector<int> vertcost, horizcost;
-vector<vector<int>> numof;
-vector<vector<bool>> visited;
+vector<vector<int>> numof, numinto;
 void dfs(int x, int y, int z){
-    if(x < 0 || x >= N || y < 0 || y >= N || visited[x][y]) return;
-    visited[x][y] = true;
+    if(x < 0 || x >= N || y < 0 || y >= N) return;
     numof[x][y] += z;
     if(dir[x][y]){
         dfs(x+1, y, z);
@@ -23,16 +22,6 @@ void dfs(int x, int y, int z){
     else{
         dfs(x, y+1, z);
     }
-}
-bool hasno(int x, int y){
-    bool works = true;
-    if(x > 0 && dir[x-1][y]){
-        works = false;
-    }
-    if(y > 0 && !dir[x][y-1]){
-        works = false;
-    }
-    return works;
 }
 int determine(){
     int cost = 0;
@@ -58,6 +47,12 @@ void update(int x, int y){
     dir[x][y] = !dir[x][y];
 }
 signed main(){
+    if(DEBUG){
+        setIO("test");
+    }
+    else{
+        setIO();
+    }
     cin >> N;
     dir.assign(N, vector<bool>(N, false));
     horizcost.assign(N, 0);
@@ -75,31 +70,44 @@ signed main(){
     }
     numof.assign(N, vector<int>(N, 1));
     visited.assign(N, vector<bool>(N, false));
+    numinto.assign(N, vector<int>(N, 0));
     queue<pair<int, int>> lookat;
     for(int i = 0; i<N; i++){
         for(int j = 0; j<N; j++){
-            if(hasno(i, j)){
+            if(numinto[i][j] == 0){
                 lookat.push({i, j});
+            }
+            if(dir[i][j]){
+                if(i < N-1){
+                    numinto[i+1][j]++;
+                }
+            }
+            else{
+                if(j < N-1){
+                    numinto[i][j+1]++;
+                }
             }
         }
     }
     while(!lookat.empty()){
         auto [x, y] = lookat.front();
         lookat.pop();
-        if(visited[x][y]){
-            continue;
-        }
-        visited[x][y] = true;
-        if(dir[x][y]){
-            if(x < N-1){
-                numof[x+1][y] += numof[x][y];
-                lookat.push({x+1, y});
+        if(!dir[x][y]){
+            if(y < N-1){
+                numof[x][y+1] += numof[x][y];
+                numinto[x][y+1]--;
+                if(numinto[x][y+1] == 0){
+                    lookat.push({x, y+1});
+                }
             }
         }
         else{
-            if(y < N-1){
-                numof[x][y+1] += numof[x][y];
-                lookat.push({x, y+1});
+            if(x < N-1){
+                numof[x+1][y] += numof[x][y];
+                numinto[x+1][y]--;
+                if(numinto[x+1][y] == 0){
+                    lookat.push({x+1, y});
+                }
             }
         }
     }
