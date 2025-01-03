9cc7457fae6a2fa15e31720208cd79cc76d6c41a
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2025-01-02 22:00:42 -0800

    ...

---
+++ pareidolia2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define DEBUG false
+#define int long long
+void setIO(string file = "") {
+    cin.tie(0)->sync_with_stdio(0);
+    if (!file.empty()) {
+        freopen((file + ".in").c_str(), "r", stdin);
+        freopen((file + ".out").c_str(), "w", stdout);
+    }
+}
+string q;
+vector<int> pfxsum;
+vector<char> bessie{'b', 'e', 's', 's', 'i', 'e'};
+signed main(){
+    cin >> q;
+    
+}
