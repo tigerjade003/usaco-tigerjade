067d46180a4980eaea2a9d9b6f0d144075036754
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2025-01-05 10:28:20 -0800

    .

---
+++ circbarngame.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void setIO(string file = "") {
+    cin.tie(0)->sync_with_stdio(0);
+    if (!file.empty()) {
+        freopen((file + ".in").c_str(), "r", stdin);
+        freopen((file + ".out").c_str(), "w", stdout);
+    }
+}
+signed main(){
+
+}
