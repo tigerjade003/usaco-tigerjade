9cf41032538a04c54956fa82be095a61b82e6281
tigerjade003 <69996929+tigerjade003@users.noreply.github.com>
2025-01-04 19:10:27 -0800

    still working on it

--- closestcow.cpp
+++ closestcow.cpp
@@ -14,18 +14,15 @@ void setIO(string file = "") {
     }
 }
 vector<pair<int, int>> grasses, earlylate;
-vector<int> pfx, Nhoj;
+map<int, int> adds, removes;
+set<int> check;
+vector<int> Nhoj, bestof;
 vector<inbetween> between;
 signed main(){
-    if(DEBUG){
-        setIO("test");
-    }
-    else{
-        setIO();
-    }
+    if(DEBUG) setIO("test");
+    else setIO();
     cin >> K >> M >> N;
     grasses.assign(K, {0, 0});
-    pfx.assign(K, 0);
     Nhoj.assign(M, 0);
     between.assign(M+1, {0, 0, 0, 0});
     earlylate.assign(K, {0, 0});
@@ -35,10 +32,6 @@ signed main(){
         grasses[i] = {a, b};
     }
     sort(grasses.begin(), grasses.end());
-    pfx[0] = grasses[0].second;
-    for(int i = 1; i<K; i++){
-        pfx[i] = pfx[i-1] + grasses[i].second;
-    }
     for(int i = 0; i<M; i++){
         cin >> Nhoj[i];
     }
@@ -57,35 +50,25 @@ signed main(){
         begin = Nhoj[i];
     }
     runningtot = 0;
-    begin = Nhoj[M-1];
     while(patches < K){
         runningtot += grasses[patches].second;
         patches++;
     }
-    between[M] = {runningtot, begin, grasses[K-1].first, 1};
-    for(int i = 0; i<M+1; i++){
-        cout << between[i].val << " " << between[i].x << " " << between[i].y << " " << between[i].needed << endl;
-    }
-    //at most I need two places to cut an entire section for farmer john --> place a cow next to the two farmer nhoj ones that border the region
-    //now I just need to determine the maximum I can win with one cow within a section
-    //if i only place 1 --> it is best to claim closer towards the person who has more on their half --> FIGURE THIS OUT MORE
-    //after doing that --> FIGURE IT OUT
-    //find the rightmost point, leftmost point of collecting each patch
+    between[M] = {runningtot, Nhoj[M-1], grasses[K-1].first, 1};
     for(int i = 0; i<K; i++){
         int leftmost, rightmost;
-        if(grasses[i].first < Nhoj[0]){
-            leftmost = 2*grasses[i].first - Nhoj[0]+1;
-        }
-        else{
-            leftmost = 2 * grasses[i].first - Nhoj[upper_bound(Nhoj.begin(), Nhoj.end(), grasses[i].first) - Nhoj.begin()] + 1;
-        }
-        if(grasses[i].first > Nhoj[M-1]){
-            rightmost = 2 * grasses[i].first - Nhoj[M-1] - 1;
-        }
-        else{
-            rightmost = 2 * grasses[i].first - Nhoj[upper_bound(Nhoj.begin(), Nhoj.end(), grasses[i].first) - Nhoj.begin()-1] -1;
-        }
+        if(grasses[i].first < Nhoj[0]) leftmost = 2*grasses[i].first - Nhoj[0]+1;
+        else leftmost = 2 * grasses[i].first - Nhoj[upper_bound(Nhoj.begin(), Nhoj.end(), grasses[i].first) - Nhoj.begin()] + 1;
+        if(grasses[i].first > Nhoj[M-1]) rightmost = 2 * grasses[i].first - Nhoj[M-1] - 1;
+        else rightmost = 2 * grasses[i].first - Nhoj[upper_bound(Nhoj.begin(), Nhoj.end(), grasses[i].first) - Nhoj.begin()-1] -1;
+        adds[leftmost]++;
+        removes[rightmost+1]++;
         earlylate[i] = {leftmost, rightmost};
+        check.insert(leftmost);
+        check.insert(rightmost);
+    }
+    bestof.assign(M+1, 0);
+    for(int i: check){
 
-    } 
+    }
 }
